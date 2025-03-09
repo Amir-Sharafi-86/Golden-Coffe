230ec460274232d4623703601fdac4a898102717
Amir-Sharafi-86 <Amirsharafi8899@gmail.com>
2025-03-09 17:24:16 +0330

    cart box

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+const toggleThemeBtn = document.querySelector("#toggle-theme");
+toggleThemeBtn.addEventListener("click" , function() {
+    if(localStorage.theme === "dark") {
+        document.documentElement.classList.remove("dark")
+        localStorage.theme = 'light'
+    }
+    else{
+        document.documentElement.classList.add("dark")
+        localStorage.setItem("theme" , "dark")
+    }
+})
