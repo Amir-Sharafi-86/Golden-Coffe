75d75847fbe13c8ea2de2934e5a2ce0cc03dce3b
Amir-Sharafi-86 <Amirsharafi8899@gmail.com>
2025-03-10 13:41:28 +0330

    js toggle menu

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,7 @@
 const toggleThemeBtn = document.querySelector(".toggle-theme");
+const subMenuBtn = document.getElementById("subMenuOpen-btn");
+const subMenu = document.querySelector(".Submenu");
+const navicon = document.querySelector(".nav-icon");
 toggleThemeBtn.addEventListener("click" , function() {
     if(localStorage.theme === "dark") {
         document.documentElement.classList.remove("dark")
@@ -9,3 +12,11 @@ toggleThemeBtn.addEventListener("click" , function() {
         localStorage.setItem("theme" , "dark")
     }
 })
+
+
+
+subMenuBtn.addEventListener('click' , (e)=> {
+    e.currentTarget.parentElement.classList.toggle("text-orange-300")
+    e.preventDefault()
+    subMenu.classList.toggle("Submenu-open")
+} )
